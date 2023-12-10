5becfa08eda42935d433494d020f0fbdc6c87bad
Mark Lisoway <lisowaym@gmail.com>
2023-12-10 05:24:01 -0700

    Clean up day 10

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -4,225 +4,98 @@ use std::fmt::{Display, Formatter};
 fn main() {
 	let input = include_str!("part_1_input.txt");
 
-	dbg!(part_1(input));
-	dbg!(part_2(input));
+	println!("Day 10, part 1: {}", part_1(input, false));
+	println!("Day 10, part 2: {}", part_2(input, false));
 }
 
-fn part_1(input: &str) -> u64 {
-	let lines: Vec<_> = input.lines().collect();
-	let mut grid = vec![vec![Node::default(); lines[0].len()]; lines.len()];
+fn part_1(input: &str, debug: bool) -> u64 {
+	let (mut grid, start) = parse(input);
 
-	let mut start = Coordinates::default();
-	for (i, line) in input.lines().enumerate() {
-		for (j, char) in line.chars().enumerate() {
-			let pipe = match char {
-				'|' => Pipe::Vertical,
-				'-' => Pipe::Horizontal,
-				'L' => Pipe::TopRight,
-				'J' => Pipe::TopLeft,
-				'F' => Pipe::BottomRight,
-				'7' => Pipe::BottomLeft,
-				'.' => Pipe::Ground,
-				'S' => Pipe::Start,
-				_ => unreachable!(),
-			};
+	let mut connection_points_to_check = VecDeque::new();
+	connection_points_to_check.push_back(start);
 
-			let node = Node {
-				c: Coordinates { x: j, y: i },
-				pipe,
-				is_main_loop: false,
-				visited: false,
-				distance: 0,
-				contained: false,
-			};
+	// Use breadth-first search to find node with longest distance from start.
 
-			if pipe == Pipe::Start {
-				start = node.c;
-			}
+	let mut max_distance = 0;
+	while let Some(point) = connection_points_to_check.pop_front() {
+		let (connection_points, distance) = {
+			let node = get_grid_node(&grid, point);
+			(find_connection_points(node), node.distance)
+		};
 
-			grid[i][j] = node;
+		for point in connection_points {
+			let visited_distance =
+				visit_node(point, &mut grid, &mut connection_points_to_check, distance);
+			max_distance = max_distance.max(visited_distance);
 		}
 	}
 
-	replace_start(start, &mut grid);
-
-	let mut to_visit = VecDeque::new();
-	to_visit.push_back(grid[start.y][start.x]);
-
-	let mut max = 0;
-	while let Some(node) = to_visit.pop_front() {
-		let connections = find_connections(node.c, &grid);
-		max = handle_node(node, connections[0], &mut grid, &mut to_visit).max(max);
-		max = handle_node(node, connections[1], &mut grid, &mut to_visit).max(max);
+	if debug {
+		draw_part_1_debug_grid(&grid, start);
 	}
 
-	// draw_grid(&grid, start);
-
-	max
+	max_distance
 }
 
-fn part_2(input: &str) -> u64 {
-	let lines: Vec<_> = input.lines().collect();
-	let mut grid = vec![vec![Node::default(); lines[0].len()]; lines.len()];
+fn part_2(input: &str, debug: bool) -> u64 {
+	let (mut grid, start) = parse(input);
 
-	let mut start = Coordinates::default();
-	for (i, line) in input.lines().enumerate() {
-		for (j, char) in line.chars().enumerate() {
-			let pipe = match char {
-				'|' => Pipe::Vertical,
-				'-' => Pipe::Horizontal,
-				'L' => Pipe::TopRight,
-				'J' => Pipe::TopLeft,
-				'F' => Pipe::BottomRight,
-				'7' => Pipe::BottomLeft,
-				'.' => Pipe::Ground,
-				'S' => Pipe::Start,
-				_ => unreachable!(),
-			};
+	walk_main_loop(&mut grid, start);
+	let contained_nodes = ray_trace_loop(&mut grid);
 
-			let node = Node {
-				c: Coordinates { x: j, y: i },
-				pipe,
-				is_main_loop: false,
-				visited: false,
-				distance: 0,
-				contained: false,
-			};
-
-			if pipe == Pipe::Start {
-				start = node.c;
-			}
-
-			grid[i][j] = node;
-		}
+	if debug {
+		draw_part_2_debug_grid(&grid);
 	}
 
-	replace_start(start, &mut grid);
-
-	let mut current = start;
-	loop {
-		let connected = find_connections(current, &grid);
-		let one = walk_node(connected[0], &mut grid);
-
-		if one {
-			current = connected[0];
-			continue;
-		}
-
-		let two = walk_node(connected[1], &mut grid);
-
-		if two {
-			current = connected[1];
-			continue;
-		}
-
-		break;
-	}
-
-	grid.iter()
-		.flatten()
-		.filter(|n| n.is_main_loop)
-		.for_each(|node| {});
-
-	let mut hits = vec![];
-
-	let mut inside_count = 0;
-	grid.iter().for_each(|row| {
-		let mut inside = false;
-
-		row.iter().for_each(|node| {
-			let pipe = node.pipe;
-
-			if !node.is_main_loop && inside {
-				hits.push(node.c);
-				inside_count += 1;
-			} else if node.is_main_loop
-				&& (pipe == Pipe::TopLeft || pipe == Pipe::TopRight || pipe == Pipe::Vertical)
-			{
-				inside = !inside;
-			}
-		});
-	});
-
-	for hit in hits {
-		let node = grid.get_mut(hit.y).expect("").get_mut(hit.x).expect("");
-		node.contained = true;
-	}
-
-	// draw_grid(&grid, start);
-
-	inside_count
+	contained_nodes
 }
 
-fn handle_node(
-	parent: Node,
-	c: Coordinates,
-	grid: &mut [Vec<Node>],
-	queue: &mut VecDeque<Node>,
-) -> u64 {
-	let node = grid.get_mut(c.y).unwrap().get_mut(c.x).unwrap();
+fn parse(input: &str) -> (Vec<Vec<Node>>, Point) {
+	let mut start = Point::default();
 
-	if node.visited {
-		return node.distance as u64;
-	}
+	let mut grid: Vec<_> = input
+		.lines()
+		.enumerate()
+		.map(|(y, line)| {
+			line.chars()
+				.enumerate()
+				.map(|(x, c)| {
+					let pipe = match c {
+						'|' => Pipe::Vertical,
+						'-' => Pipe::Horizontal,
+						'L' => Pipe::TopRight,
+						'J' => Pipe::TopLeft,
+						'F' => Pipe::BottomRight,
+						'7' => Pipe::BottomLeft,
+						'.' => Pipe::Ground,
+						'S' => Pipe::Start,
+						_ => unreachable!("No other grid characters"),
+					};
 
-	node.distance = parent.distance + 1;
-	node.visited = true;
-	queue.push_back(*node);
+					if pipe == Pipe::Start {
+						start.x = x;
+						start.y = y;
+					}
 
-	node.distance as u64
+					Node {
+						point: Point { x, y },
+						pipe,
+						visited: false,
+						distance: 0,
+						is_main_loop: false,
+						contained: false,
+					}
+				})
+				.collect()
+		})
+		.collect();
+
+	replace_start_new(&mut grid, start);
+
+	(grid, start)
 }
 
-fn walk_node(c: Coordinates, grid: &mut [Vec<Node>]) -> bool {
-	let node = grid.get_mut(c.y).unwrap().get_mut(c.x).unwrap();
-
-	if node.is_main_loop {
-		return false;
-	}
-
-	node.is_main_loop = true;
-	true
-}
-
-#[derive(Copy, Clone, Debug, Default)]
-struct Coordinates {
-	pub x: usize,
-	pub y: usize,
-}
-
-#[derive(Copy, Clone, Debug, Default)]
-struct Node {
-	pub c: Coordinates,
-	pub pipe: Pipe,
-	pub is_main_loop: bool,
-	pub visited: bool,
-	pub distance: usize,
-	pub contained: bool,
-}
-
-fn draw_grid(nodes: &[Vec<Node>], start: Coordinates) {
-	for (y, node) in nodes.iter().enumerate() {
-		let line: Vec<_> = node
-			.iter()
-			.enumerate()
-			.map(|(x, n)| {
-				let a = match n.is_main_loop {
-					true => n.pipe.to_string(),
-					false => match n.contained {
-						true => "0".to_string(),
-						false => " ".to_string(),
-					},
-				};
-				format!("{:>3}", a)
-			})
-			.collect();
-		let line = line.join("");
-
-		println!("{line}");
-	}
-}
-
-fn replace_start(start: Coordinates, grid: &mut [Vec<Node>]) {
+fn replace_start_new(grid: &mut [Vec<Node>], start: Point) {
 	let can_connect_top = match start.y.checked_sub(1) {
 		Some(y) => match grid.get(y).and_then(|r| r.get(start.x)) {
 			Some(node) => node.pipe.can_connect_bottom(),
@@ -267,43 +140,229 @@ fn replace_start(start: Coordinates, grid: &mut [Vec<Node>]) {
 		(false, true, true, false) => Pipe::BottomRight,
 		(false, true, false, true) => Pipe::Horizontal,
 		(false, false, true, true) => Pipe::BottomLeft,
-		_ => unreachable!(),
+		_ => unreachable!("We are guaranteed to have 2 connections to start node"),
 	};
 
-	let start = grid.get_mut(start.y).expect("").get_mut(start.x).expect("");
+	let start = get_grid_node_mut(grid, start);
 	start.pipe = pipe;
 }
 
-fn find_connections(c: Coordinates, grid: &[Vec<Node>]) -> [Coordinates; 2] {
-	let node = grid[c.y][c.x];
+fn get_grid_node(grid: &[Vec<Node>], point: Point) -> &Node {
+	grid.get(point.y)
+		.expect("Row to exist")
+		.get(point.x)
+		.expect("Column to exist")
+}
+
+fn get_grid_node_mut(grid: &mut [Vec<Node>], point: Point) -> &mut Node {
+	grid.get_mut(point.y)
+		.expect("Row to exist")
+		.get_mut(point.x)
+		.expect("Column to exist")
+}
+
+fn visit_node(
+	point: Point,
+	grid: &mut [Vec<Node>],
+	queue: &mut VecDeque<Point>,
+	distance: usize,
+) -> u64 {
+	let node = get_grid_node_mut(grid, point);
+
+	if node.visited {
+		return node.distance as u64;
+	}
+
+	node.distance = distance + 1;
+	node.visited = true;
+	queue.push_back(point);
+
+	node.distance as u64
+}
+
+fn find_connection_points(node: &Node) -> [Point; 2] {
+	let point = node.point;
 
 	match node.pipe {
 		Pipe::Vertical => [
-			Coordinates { x: c.x, y: c.y - 1 },
-			Coordinates { x: c.x, y: c.y + 1 },
+			Point {
+				x: point.x,
+				y: point.y - 1,
+			},
+			Point {
+				x: point.x,
+				y: point.y + 1,
+			},
 		],
 		Pipe::Horizontal => [
-			Coordinates { x: c.x - 1, y: c.y },
-			Coordinates { x: c.x + 1, y: c.y },
+			Point {
+				x: point.x - 1,
+				y: point.y,
+			},
+			Point {
+				x: point.x + 1,
+				y: point.y,
+			},
 		],
 		Pipe::TopRight => [
-			Coordinates { x: c.x, y: c.y - 1 },
-			Coordinates { x: c.x + 1, y: c.y },
+			Point {
+				x: point.x,
+				y: point.y - 1,
+			},
+			Point {
+				x: point.x + 1,
+				y: point.y,
+			},
 		],
 		Pipe::TopLeft => [
-			Coordinates { x: c.x, y: c.y - 1 },
-			Coordinates { x: c.x - 1, y: c.y },
+			Point {
+				x: point.x,
+				y: point.y - 1,
+			},
+			Point {
+				x: point.x - 1,
+				y: point.y,
+			},
 		],
 		Pipe::BottomRight => [
-			Coordinates { x: c.x, y: c.y + 1 },
-			Coordinates { x: c.x + 1, y: c.y },
+			Point {
+				x: point.x,
+				y: point.y + 1,
+			},
+			Point {
+				x: point.x + 1,
+				y: point.y,
+			},
 		],
 		Pipe::BottomLeft => [
-			Coordinates { x: c.x, y: c.y + 1 },
-			Coordinates { x: c.x - 1, y: c.y },
+			Point {
+				x: point.x,
+				y: point.y + 1,
+			},
+			Point {
+				x: point.x - 1,
+				y: point.y,
+			},
 		],
-		Pipe::Ground | Pipe::Start => unreachable!(),
+		Pipe::Ground | Pipe::Start => unreachable!("These don't connect to anything"),
 	}
+}
+
+/// Walk around the entire loop from the given start point, and mark
+/// all nodes along the path as part of the main loop.
+fn walk_main_loop(grid: &mut [Vec<Node>], start: Point) {
+	let mut current = start;
+
+	'outer: loop {
+		let connection_points = {
+			let node = get_grid_node(grid, current);
+			find_connection_points(node)
+		};
+
+		for point in connection_points {
+			if walk_node_new(grid, point) {
+				current = point;
+				continue 'outer;
+			}
+		}
+
+		break;
+	}
+}
+
+fn walk_node_new(grid: &mut [Vec<Node>], point: Point) -> bool {
+	let node = get_grid_node_mut(grid, point);
+
+	match node.is_main_loop {
+		true => false,
+		false => {
+			node.is_main_loop = true;
+			true
+		}
+	}
+}
+
+fn ray_trace_loop(grid: &mut [Vec<Node>]) -> u64 {
+	grid.iter_mut()
+		.map(|row| {
+			let mut inside = false;
+			let mut inside_count = 0;
+
+			row.iter_mut().for_each(|node| {
+				let pipe = node.pipe;
+
+				if !node.is_main_loop && inside {
+					node.contained = true;
+					inside_count += 1;
+				} else if node.is_main_loop
+					&& (pipe == Pipe::TopLeft || pipe == Pipe::TopRight || pipe == Pipe::Vertical)
+				{
+					inside = !inside;
+				}
+			});
+
+			inside_count
+		})
+		.sum()
+}
+
+fn draw_part_1_debug_grid(grid: &[Vec<Node>], start: Point) {
+	for (y, node) in grid.iter().enumerate() {
+		let line: Vec<_> = node
+			.iter()
+			.enumerate()
+			.map(|(x, node)| {
+				let value = match x == start.x && y == start.y {
+					true => "S".to_string(),
+					false => match node.pipe == Pipe::Ground {
+						true => ".".to_string(),
+						false => node.distance.to_string(),
+					},
+				};
+
+				format!("{:>3}", value)
+			})
+			.collect();
+		let line = line.join("");
+
+		println!("{line}");
+	}
+}
+
+fn draw_part_2_debug_grid(grid: &[Vec<Node>]) {
+	for node in grid.iter() {
+		let line: Vec<_> = node
+			.iter()
+			.map(|node| {
+				let a = match node.is_main_loop {
+					true => node.pipe.to_string(),
+					false => match node.contained {
+						true => "0".to_string(),
+						false => " ".to_string(),
+					},
+				};
+				format!("{:>3}", a)
+			})
+			.collect();
+		let line = line.join("");
+
+		println!("{line}");
+	}
+}
+
+#[derive(Copy, Clone, Default)]
+struct Point {
+	pub x: usize,
+	pub y: usize,
+}
+
+struct Node {
+	pub point: Point,
+	pub pipe: Pipe,
+	pub visited: bool,
+	pub distance: usize,
+	pub is_main_loop: bool,
+	pub contained: bool,
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
@@ -316,21 +375,6 @@ enum Pipe {
 	BottomLeft,
 	Ground,
 	Start,
-}
-
-#[derive(Default, Copy, Clone, Debug, Eq, PartialEq)]
-struct Cell {
-	pub blocked_top: bool,
-	pub blocked_right: bool,
-	pub blocked_bottom: bool,
-	pub blocked_left: bool,
-	pub pipe: Pipe,
-}
-
-impl Display for Cell {
-	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-		self.pipe.fmt(f)
-	}
 }
 
 impl Pipe {
@@ -387,12 +431,6 @@ impl Pipe {
 	}
 }
 
-impl Default for Pipe {
-	fn default() -> Self {
-		Self::Ground
-	}
-}
-
 impl Display for Pipe {
 	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
 		match self {
@@ -423,14 +461,14 @@ LJ...
 "#
 		.trim();
 
-		assert_eq!(part_1(input), 8);
+		assert_eq!(part_1(input, false), 8);
 	}
 
 	#[test]
 	fn part_01_test() {
 		let input = include_str!("part_1_input.txt");
 
-		assert_eq!(part_1(input), 6903);
+		assert_eq!(part_1(input, false), 6903);
 	}
 
 	#[test]
@@ -449,13 +487,13 @@ L7JLJL-JLJLJL--JLJ.L
 "#
 		.trim();
 
-		assert_eq!(part_2(input), 10);
+		assert_eq!(part_2(input, false), 10);
 	}
 
 	#[test]
 	fn part_02_test() {
 		let input = include_str!("part_1_input.txt");
 
-		assert_eq!(part_2(input), 265);
+		assert_eq!(part_2(input, false), 265);
 	}
 }
